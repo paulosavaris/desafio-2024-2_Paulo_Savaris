014898aad0bb9e38751f00151f817f130d86fe12
Paulo Savaris <pauloosavaris@gmail.com>
2024-11-18 11:42:05 -0300

    Spring Initializr

---
+++ 2024-2/src/test/java/unoesc/desafio/_42/ApplicationTests.java
@@ -0,0 +1,13 @@
+package unoesc.desafio._42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
